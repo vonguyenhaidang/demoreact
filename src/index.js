757bc0080f5132ebe3292efc6405658d4dce3e8d
vonguyenhaidang <haidang16699@gmail.com>
2023-02-27 10:20:11 +0700

    Update

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import IndexPage from './pages/MainPage';
+import MenList from './pages/MenWatches';
+import WomenList from './pages/WomenWatches';
+import PageSlide from './pages/Slideshow';
+import PageNotFound from './pages/PageNotFound';
+import ProductSingle from './pages/ProductSingle';
+import CartPage from './pages/Cart';
+export default function HomePage() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/index.html" element={<Layout />}>
+          <Route index element={<IndexPage />}></Route>
+        </Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />}></Route>
+          <Route path="single" element={<ProductSingle />}></Route>
+          <Route path='cart' element={<CartPage />}></Route>
+        </Route>
+        <Route path="/products" element={<Layout />}>
+          <Route path="men" element={<MenList />}></Route>
+          <Route path="women" element={<WomenList />}></Route>
+          <Route path="slide" element={<PageSlide />}></Route>
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter >
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HomePage />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
